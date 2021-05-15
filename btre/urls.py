55044415f870b8f7913ffae723df2d69348d94a9
aofwittawat <aofwittawat@gmail.com>
2021-05-15 14:48:17 +0700

    First step

---
+++ btre/urls.py
@@ -0,0 +1,9 @@
+import pages
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('pages.urls')),
+    path('admin/', admin.site.urls),
+    path('listings/', include('listings.urls')),
+]
